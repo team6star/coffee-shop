eca6851c617e551cb2e6d885d3d8159301f6b88d
star <2037631972@qq.com>
2025-04-22 21:47:31 +0800

    feat:商品详情-准备工作/页面渲染

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -72,7 +72,7 @@ onLoad(async () => {
               :key="goods.id"
               class="goods"
               hover-class="none"
-              :url="`/pages/goods/goods?id=goods.id`"
+              :url="`/pages/goods/goods?id=${goods.id}`"
             >
               <image class="image" :src="goods.picture"></image>
               <view class="name ellipsis">{{ goods.name }}</view>
