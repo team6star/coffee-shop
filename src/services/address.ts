b526bf15cff3dae1b57c919a889c49228983dfc8
star <2037631972@qq.com>
2025-04-26 11:57:58 +0800

    feat:地址模块-新建地址/列表渲染

---
+++ src/services/address.ts
@@ -0,0 +1,22 @@
+import type { AddressItem, AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+/**  添加收货地址
+ * @return data 请求参数
+ */
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
+/**
+ *  获取收货地址列表
+ 
+ *  */
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
+  })
+}
