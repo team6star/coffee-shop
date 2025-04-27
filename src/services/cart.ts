ec4c7bf693d9c6835e9c3af6a930b82c41063307
star <2037631972@qq.com>
2025-04-27 22:14:46 +0800

    feat:购物车列表/分页加载

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,12 +1,22 @@
+import type { CartItem } from '@/types/cart'
 import { http } from '@/utils/http'
 /**
  * 加入购物车
  * @params data - 商品信息 请求体参数
  * */
 export const postMemberCartAPI = (data: { skuId: string; count: number }) => {
-  return http({
+  return http<CartItem>({
     method: 'POST',
     url: '/member/cart',
     data,
+  })
+}
+/* 
+获取购物车列表
+*/
+export const getMemberCartAPI = () => {
+  return http<CartItem[]>({
+    method: 'GET',
+    url: '/member/cart',
   })
 }
